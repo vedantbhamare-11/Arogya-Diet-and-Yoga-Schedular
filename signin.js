a557148ea4076644d81df641401c5f5dde3a57c4
sarang <sarangsathiandran@gmail.com>
2021-09-10 22:29:59 +0530

    added 'show password'

--- signin.js
+++ signin.js
@@ -1,3 +1,4 @@
+// signup switch
 let left_cover = document.getElementById("left-cover");
 let left_form = document.getElementById("left-form");
 
@@ -12,4 +13,41 @@ function switchSignup() {
   left_cover.classList.remove("cover-hide");
   right_form.classList.remove("form-hide");
   right_cover.classList.add("cover-hide");
+}
+// show password switch
+let state = false;
+function showPass1() {
+  if (state) {
+    document.getElementById("signin-password").setAttribute("type", "password");
+    document.getElementById("eye1").style.color = "#7a797e";
+    state = false;
+  } else {
+    document.getElementById("signin-password").setAttribute("type", "text");
+    document.getElementById("eye1").style.color = "#799741";
+    state = true;
+  }
+}
+function showPass2() {
+  if (state) {
+    document.getElementById("signup-password").setAttribute("type", "password");
+    document.getElementById("eye2").style.color = "#7a797e";
+    state = false;
+  } else {
+    document.getElementById("signup-password").setAttribute("type", "text");
+    document.getElementById("eye2").style.color = "#799741";
+    state = true;
+  }
+}
+function showPass3() {
+  if (state) {
+    document
+      .getElementById("confirm-password")
+      .setAttribute("type", "password");
+    document.getElementById("eye3").style.color = "#7a797e";
+    state = false;
+  } else {
+    document.getElementById("confirm-password").setAttribute("type", "text");
+    document.getElementById("eye3").style.color = "#799741";
+    state = true;
+  }
 }
