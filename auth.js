0fa5c3b64a17c4310773d86b0193db36ee6ac4a1
sarang <sarangsathiandran@gmail.com>
2021-09-29 18:36:41 +0530

    logout

--- auth.js
+++ auth.js
@@ -33,3 +33,11 @@ signinForm.addEventListener("submit", (e) => {
     console.log(cred.user);
   });
 });
+// logout
+const logout = documment.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("user signed out");
+  });
+});
