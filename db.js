6f05bc09f35a4bceb7c48cbc986c28763286e2af
sarang <sarangsathiandran@gmail.com>
2021-10-04 22:20:22 +0530

    db inputs done

--- db.js
+++ db.js
@@ -5,26 +5,20 @@ auth.onAuthStateChanged((user) => {
     profileForm.addEventListener("submit", (e) => {
       e.preventDefault();
       alert("function is working");
-      return db.collection("users").doc(user.uid).set({
-        name: profileForm["name"].value,
-        height: profileForm["height"].value,
-        weight: profileForm["weight"].value,
-        age: profileForm["age"].value,
-        // gender: profileForm["gender"].value,
-        // meal: profileForm["meal"].value,
-      });
+      return db
+        .collection("users")
+        .doc(user.uid)
+        .set({
+          name: profileForm["name"].value,
+          height: profileForm["height"].value,
+          weight: profileForm["weight"].value,
+          age: profileForm["age"].value,
+          gender: document.querySelector('input[name="radio"]:checked').value,
+          meal: document.querySelector('input[name="meal"]:checked').value,
+        });
     });
     console.log("user signed in", user);
   } else {
     console.log("user signed out");
   }
 });
-let gender = document.getElementById("userGender").value;
-let gender_value;
-if (gender == "male") {
-  gender_value = document.getElementById("male").value;
-} else if (gender == "female") {
-  gender_value = document.getElementById("female").value;
-}
-
-console.log(gender_value);
