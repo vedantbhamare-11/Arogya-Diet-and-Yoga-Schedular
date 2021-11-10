bdf4eba0f2c928a5a77c68a25de9271c86a66bd3
sarang <sarangsathiandran@gmail.com>
2021-11-10 18:27:09 +0530

    last

--- pointer.js
+++ pointer.js
@@ -7,40 +7,40 @@ var i = auth.onAuthStateChanged((user) => {
         const bmiValue = doc.data().BMI;
         var ele = document.getElementById("pointer");
         switch (true) {
-          case bmiValue < 14 && bmiValue >= 12:
+          case bmiValue <= 14 && bmiValue >= 12:
             ele.style.left = "4%";
             break;
-          case bmiValue < 17 && bmiValue >= 15:
+          case bmiValue <= 17 && bmiValue >= 15:
             ele.style.left = "8%";
             break;
-          case bmiValue < 19 && bmiValue >= 18:
+          case bmiValue <= 19 && bmiValue >= 18:
             ele.style.left = "12%";
             break;
-          case bmiValue < 22 && bmiValue >= 20:
+          case bmiValue <= 22 && bmiValue >= 20:
             ele.style.left = "17%";
             break;
-          case bmiValue < 24 && bmiValue >= 23:
+          case bmiValue <= 24 && bmiValue >= 23:
             ele.style.left = "20%";
             break;
-          case bmiValue < 26 && bmiValue >= 25:
+          case bmiValue <= 26 && bmiValue >= 25:
             ele.style.left = "25%";
             break;
-          case bmiValue < 29 && bmiValue >= 27:
+          case bmiValue <= 29 && bmiValue >= 27:
             ele.style.left = "28%";
             break;
-          case bmiValue < 33 && bmiValue >= 30:
+          case bmiValue <= 33 && bmiValue >= 30:
             ele.style.left = "35%";
             break;
-          case bmiValue < 36 && bmiValue >= 34:
+          case bmiValue <= 36 && bmiValue >= 34:
             ele.style.left = "40%";
             break;
-          case bmiValue < 39 && bmiValue >= 37:
+          case bmiValue <= 39 && bmiValue >= 37:
             ele.style.left = "45%";
             break;
-          case bmiValue < 43 && bmiValue >= 40:
+          case bmiValue <= 43 && bmiValue >= 40:
             ele.style.left = "53%";
             break;
-          case bmiValue < 47 && bmiValue >= 44:
+          case bmiValue <= 47 && bmiValue >= 44:
             ele.style.left = "57%";
             break;
           default:
