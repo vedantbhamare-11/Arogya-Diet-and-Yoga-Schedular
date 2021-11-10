4a60cd25320668ba68002a4815b5d9f3c1b2d447
sarang <sarangsathiandran@gmail.com>
2021-11-10 18:14:04 +0530

    pointer fix

--- pointer.js
+++ pointer.js
@@ -6,49 +6,40 @@ var i = auth.onAuthStateChanged((user) => {
       .then((doc) => {
         const bmiValue = doc.data().BMI;
         var ele = document.getElementById("pointer");
-        //under weight
-        if (bmiValue < 14 && bmiValue >= 12) {
-          ele.style.left = "4%";
-        }
-        if (bmiValue < 17 && bmiValue >= 15) {
-          ele.style.left = "8%";
-        }
-        if (bmiValue < 19 && bmiValue >= 18) {
-          ele.style.left = "12%";
-        }
-        //healthy weight
-        if (bmiValue < 22 && bmiValue >= 20) {
-          ele.style.left = "17%";
-        }
-        if (bmiValue < 24 && bmiValue >= 23) {
-          ele.style.left = "20%";
-        }
-        //over weight
-        if (bmiValue < 25 && bmiValue >= 26) {
-          ele.style.left = "25%";
-        }
-        if (bmiValue < 27 && bmiValue >= 29) {
-          ele.style.left = "28%";
-        }
-        //significantly over weight
-        if (bmiValue < 33 && bmiValue >= 30) {
-          ele.style.left = "35%";
-        }
-        if (bmiValue < 36 && bmiValue >= 34) {
-          ele.style.left = "40%";
-        }
-        if (bmiValue < 39 && bmiValue >= 37) {
-          ele.style.left = "45%";
-        }
-        //Dangerously over weight
-        if (bmiValue < 43 && bmiValue >= 40) {
-          ele.style.left = "53%";
-        }
-        if (bmiValue < 47 && bmiValue >= 44) {
-          ele.style.left = "57%";
-        }
-        if (bmiValue < 51 && bmiValue >= 48) {
-          ele.style.left = "62%";
+        switch (true) {
+          case bmiValue < 14 && bmiValue >= 12:
+            ele.style.left = "4%";
+            break;
+          case bmiValue < 17 && bmiValue >= 15:
+            ele.style.left = "8%";
+            break;
+          case bmiValue < 19 && bmiValue >= 18:
+            ele.style.left = "12%";
+            break;
+          case bmiValue < 22 && bmiValue >= 20:
+            ele.style.left = "17%";
+            break;
+          case bmiValue < 24 && bmiValue >= 23:
+            ele.style.left = "20%";
+            break;
+          case bmiValue < 26 && bmiValue >= 25:
+            ele.style.left = "25%";
+            break;
+          case bmiValue < 29 && bmiValue >= 27:
+            ele.style.left = "28%";
+            break;
+          case bmiValue < 33 && bmiValue >= 30:
+            ele.style.left = "35%";
+            break;
+          case bmiValue < 36 && bmiValue >= 34:
+            ele.style.left = "40%";
+            break;
+          case bmiValue < 39 && bmiValue >= 37:
+            ele.style.left = "45%";
+            break;
+          case bmiValue < 43 && bmiValue >= 40:
+            ele.style.left = "53%";
+            break;
         }
       });
   }
